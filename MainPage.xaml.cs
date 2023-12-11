d192e4acec1f6a9adb39ff3c17c06564dc064a44
Thomr77 <96273713+Thomr77@users.noreply.github.com>
2023-12-11 16:20:04 +1300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorNoRootPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBlazorNoRootPage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
